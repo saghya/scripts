afd01d4ad6db52f165ab5f1bd91cb89af12f63d2
saghya <saghyattila77@gmail.com>
2022-11-26 23:26:33 +0100

    theme switching

---
+++ darkTheme.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# dwm
+sed -i "s/#EFF1F5/#1E1E2E/g" ~/.local/src/dwm/config.def.h
+sed -i "s/#BCC0CC/#45475A/g" ~/.local/src/dwm/config.def.h
+sed -i "s/#4C4F69/#CDD6F4/g" ~/.local/src/dwm/config.def.h
+sed -i "s/#7287FD/#B4BEFE/g" ~/.local/src/dwm/config.def.h
+cd ~/.local/src/dwm/ && sudo make clean install && killall dwm
+
+# blocks
+sed -i "s/#40A02B/#A6E3A1/g" ~/.local/scripts/blocks/*
+sed -i "s/#1E66F5/#89B4FA/g" ~/.local/scripts/blocks/*
+sed -i "s/#EA76CB/#F5C2E7/g" ~/.local/scripts/blocks/*
+sed -i "s/#DF8E1D/#F9E2AF/g" ~/.local/scripts/blocks/*
+sed -i "s/#D20F39/#F38BA8/g" ~/.local/scripts/blocks/*
+sed -i "s/#179299/#94E2D5/g" ~/.local/scripts/blocks/*
+killall dwmblocks && dwmblocks &
+
+# gtk
+sed -i "s/gtk-theme-name=.*/gtk-theme-name=\"Catppuccin-Mocha-Grey\"/g" ~/.gtkrc-2.0
+sed -i "s/gtk-icon-theme-name=.*/gtk-icon-theme-name=\"Papirus-Dark\"/g" ~/.gtkrc-2.0
+sed -i "s/gtk-theme-name=.*/gtk-theme-name=\"Catppuccin-Mocha-Grey\"/g" ~/.config/gtk-3.0/settings.ini
+sed -i "s/gtk-icon-theme-name=.*/gtk-icon-theme-name=Papirus-Dark/g" ~/.config/gtk-3.0/settings.ini
+
+# qt
+sed -i "s/theme=.*/theme=Catppuccin-Mocha-Lavender/g" ~/.config/Kvantum/kvantum.kvconfig
+
+# alacritty
+sed -i "s/~\/.config\/alacritty\/.*/~\/.config\/alacritty\/catppuccin\/catppuccin-mocha.yml/g" ~/.config/alacritty/alacritty.yml
+
+# neovim
+sed -i "s/local colorscheme =.*/local colorscheme = \"catppuccin-mocha\"/g" ~/.config/nvim/lua/user/colorscheme.lua
+
+# dmenu
+sed -i "s/#EFF1F5/#1E1E2E/g" ~/.local/src/dmenu/config.def.h
+sed -i "s/#4C4F69/#CDD6F4/g" ~/.local/src/dmenu/config.def.h
+sed -i "s/#7287FD/#B4BEFE/g" ~/.local/src/dmenu/config.def.h
+cd ~/.local/src/dmenu/ && sudo make clean install && killall dmenu
+
+# dunst
+sed -i "s/#EFF1F5/#1E1E2E/g" ~/.config/dunst/dunstrc
+sed -i "s/#4C4F69/#CDD6F4/g" ~/.config/dunst/dunstrc
+sed -i "s/#7287FD/#B4BEFE/g" ~/.config/dunst/dunstrc
+sed -i "s/#D20F39/#F38BA8/g" ~/.config/dunst/dunstrc
+sed -i "s/#BCC0CC/#45475A/g" ~/.config/dunst/dunstrc
+sed -i "s/Papirus-Light/Papirus-Dark/g" ~/.config/dunst/dunstrc
+killall dunst
+
---
+++ lightTheme.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# dwm
+sed -i "s/#1E1E2E/#EFF1F5/g" ~/.local/src/dwm/config.def.h
+sed -i "s/#45475A/#BCC0CC/g" ~/.local/src/dwm/config.def.h
+sed -i "s/#CDD6F4/#4C4F69/g" ~/.local/src/dwm/config.def.h
+sed -i "s/#B4BEFE/#7287FD/g" ~/.local/src/dwm/config.def.h
+cd ~/.local/src/dwm/ && sudo make clean install && killall dwm
+
+# blocks
+sed -i "s/#A6E3A1/#40A02B/g" ~/.local/scripts/blocks/*
+sed -i "s/#89B4FA/#1E66F5/g" ~/.local/scripts/blocks/*
+sed -i "s/#F5C2E7/#EA76CB/g" ~/.local/scripts/blocks/*
+sed -i "s/#F9E2AF/#DF8E1D/g" ~/.local/scripts/blocks/*
+sed -i "s/#F38BA8/#D20F39/g" ~/.local/scripts/blocks/*
+sed -i "s/#94E2D5/#179299/g" ~/.local/scripts/blocks/*
+killall dwmblocks && dwmblocks &
+
+# gtk
+sed -i "s/gtk-theme-name=.*/gtk-theme-name=\"Catppuccin-Latte-Grey\"/g" ~/.gtkrc-2.0
+sed -i "s/gtk-icon-theme-name=.*/gtk-icon-theme-name=\"Papirus-Light\"/g" ~/.gtkrc-2.0
+sed -i "s/gtk-theme-name=.*/gtk-theme-name=Catppuccin-Latte-Grey/g" ~/.config/gtk-3.0/settings.ini
+sed -i "s/gtk-icon-theme-name=.*/gtk-icon-theme-name=Papirus-Light/g" ~/.config/gtk-3.0/settings.ini
+
+# qt
+sed -i "s/theme=.*/theme=Catppuccin-Latte-Lavender/g" ~/.config/Kvantum/kvantum.kvconfig
+
+# alacritty
+sed -i "s/~\/.config\/alacritty\/.*/~\/.config\/alacritty\/catppuccin\/catppuccin-latte.yml/g" ~/.config/alacritty/alacritty.yml
+
+# neovim
+sed -i "s/local colorscheme =.*/local colorscheme = \"catppuccin-latte\"/g" ~/.config/nvim/lua/user/colorscheme.lua
+
+# dmenu
+sed -i "s/#1E1E2E/#EFF1F5/g" ~/.local/src/dmenu/config.def.h
+sed -i "s/#CDD6F4/#4C4F69/g" ~/.local/src/dmenu/config.def.h
+sed -i "s/#B4BEFE/#7287FD/g" ~/.local/src/dmenu/config.def.h
+cd ~/.local/src/dmenu/ && sudo make clean install && killall dmenu
+
+# dunst
+sed -i "s/#1E1E2E/#EFF1F5/g" ~/.config/dunst/dunstrc
+sed -i "s/#CDD6F4/#4C4F69/g" ~/.config/dunst/dunstrc
+sed -i "s/#B4BEFE/#7287FD/g" ~/.config/dunst/dunstrc
+sed -i "s/#F38BA8/#D20F39/g" ~/.config/dunst/dunstrc
+sed -i "s/#45475A/#BCC0CC/g" ~/.config/dunst/dunstrc
+sed -i "s/Papirus-Dark/Papirus-Light/g" ~/.config/dunst/dunstrc
+killall dunst
+
