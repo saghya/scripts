09dfa25ead1f625a15f0ee9cafe1e1ede9673bc1
saghya <saghyattila77@gmail.com>
2022-12-10 12:42:47 +0100

    removed qt5ct

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ packages() {
         man-pages polkit acpid pipewire pipewire-pulse pipewire-alsa pavucontrol pamixer wget xautolock
         udiskie alacritty noto-fonts noto-fonts-cjk noto-fonts-extra ttf-font-awesome ttf-jetbrains-mono
         ttf-ubuntu-font-family dunst feh dash zsh zsh-autosuggestions maim neovim picom lxappearance
-        gtk-engine-murrine gnome-themes-extra papirus-icon-theme kvantum qt5ct qt6ct ueberzug ranger
+        gtk-engine-murrine gnome-themes-extra papirus-icon-theme kvantum qt6ct ueberzug ranger
         pcmanfm zathura zathura-pdf-mupdf mpv exa inetutils ripgrep fd pyright bluez bluez-utils
         python-pygments networkmanager dnsmasq cups libhandy system-config-printer hplip"
     sudo pacman --noconfirm -Syyu
