abb71c98261be9e5872da38b046412cd485a5369
saghya <saghyattila77@gmail.com>
2026-04-06 13:01:42 +0200

    wayland

--- install.sh
+++ install.sh
@@ -2,6 +2,76 @@
 
 error() { printf "%s\n" "$1" >> ~/.install-errors.log; }
 
+common_packages="base-devel clang gdb python ntfs-3g openssh xdg-utils man-db man-pages
+    polkit acpid pipewire pipewire-pulse pipewire-alsa pavucontrol pamixer wget udiskie
+    alacritty noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
+    ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-ubuntu-font-family dunst dash zsh
+    zsh-autosuggestions neovim lxappearance gnome-themes-extra papirus-icon-theme kvantum
+    kvantum-qt5 qt6ct ueberzugpp ranger pcmanfm zathura zathura-pdf-mupdf mpv eza
+    inetutils ripgrep fd pyright bluez bluez-utils python-pygments networkmanager dnsmasq
+    cups libhandy system-config-printer hplip bash-language-server bear tree-sitter-cli ly
+    unzip wireguard-tools wiremix firefox"
+
+xorg_packages="xorg-server xorg-xwininfo xorg-xinit xorg-xprop xorg-xrandr xorg-xdpyinfo
+    xclip xdotool xbindkeys xterm feh maim picom xorg-xset xss-lock"
+
+wayland_packages="wayland wayland-protocols wlroots0.19 foot fuzzel wl-clipboard fcft
+    pixman tllist hypridle hyprlock nwg-look swaybg grim slurp imv"
+
+common_aur_packages="breeze-snow-cursor-theme htop-vim dashbinsh networkmanager-dmenu-git
+    dmenu-bluetooth catppuccin-gtk-theme-mocha kvantum-theme-catppuccin-git
+    zsh-fast-syntax-highlighting hplip-plugin xidlehook"
+
+DISPLAY_STACK=""
+
+choose_display_stack() {
+    case "${1:-}" in
+        xorg|wayland|both)
+            DISPLAY_STACK=$1
+            ;;
+        "")
+            printf "1) Xorg\n"
+            printf "2) Wayland\n"
+            printf "3) Both\n"
+            printf "Choose display stack [default=3]: "
+            read -r display_stack_choice
+            case "${display_stack_choice:-3}" in
+                1) DISPLAY_STACK="xorg" ;;
+                2) DISPLAY_STACK="wayland" ;;
+                3) DISPLAY_STACK="both" ;;
+                *)
+                    printf "Invalid selection: %s\n" "$display_stack_choice" >&2
+                    exit 1
+                    ;;
+            esac
+            ;;
+        *)
+            printf "Usage: %s [xorg|wayland|both]\n" "$0" >&2
+            exit 1
+            ;;
+    esac
+}
+
+install_xorg() {
+    [ "$DISPLAY_STACK" = "xorg" ] || [ "$DISPLAY_STACK" = "both" ]
+}
+
+install_wayland() {
+    [ "$DISPLAY_STACK" = "wayland" ] || [ "$DISPLAY_STACK" = "both" ]
+}
+
+install_pacman_packages() {
+    for package in $1; do
+        sudo pacman --needed --noconfirm -S "$package" || error "Error installing $package"
+    done
+}
+
+install_yay_packages() {
+    for package in $1; do
+        yay --sudoloop --needed --noconfirm -S "$package" || error "Error installing $package"
+    done
+}
+
 ## SETUP ##
 setup() {
     [ -f ~/.install-errors.log ] && rm ~/.install-errors.log
@@ -19,19 +89,10 @@ setup() {
 
 ## PACKAGES ##
 packages() {
-    PCKGS="base-devel clang gdb python ntfs-3g openssh xorg-server xorg-xwininfo xorg-xinit
-        xorg-xprop xorg-xrandr xorg-xdpyinfo xclip xdotool xbindkeys xdg-utils xterm man-db
-        man-pages polkit acpid pipewire pipewire-pulse pipewire-alsa pavucontrol pamixer wget
-        udiskie alacritty noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-jetbrains-mono-nerd
-        ttf-jetbrains-mono ttf-ubuntu-font-family dunst feh dash zsh zsh-autosuggestions maim neovim
-        picom lxappearance gnome-themes-extra papirus-icon-theme kvantum kvantum-qt5 xorg-xset
-        qt6ct ueberzugpp ranger pcmanfm zathura zathura-pdf-mupdf mpv eza inetutils ripgrep fd pyright
-        bluez bluez-utils python-pygments networkmanager dnsmasq cups libhandy system-config-printer
-        hplip xss-lock bash-language-server bear tree-sitter-cli ly unzip wireguard-tools wiremix firefox"
     sudo pacman --noconfirm -Syyu
-    for PCKG in $PCKGS; do
-        sudo pacman --needed --noconfirm -S "$PCKG" || error "Error installing $PCKG"
-    done
+    install_pacman_packages "$common_packages"
+    install_xorg && install_pacman_packages "$xorg_packages"
+    install_wayland && install_pacman_packages "$wayland_packages"
 
     ## AUR PACKAGES ##
     # yay
@@ -43,12 +104,7 @@ packages() {
         error "Error installing yay"
     fi
 
-    AUR_PCKGS="breeze-snow-cursor-theme htop-vim dashbinsh networkmanager-dmenu-git
-        dmenu-bluetooth catppuccin-gtk-theme-mocha catppuccin-gtk-theme-latte kvantum-theme-catppuccin-git
-        zsh-fast-syntax-highlighting hplip-plugin xidlehook"
-    for PCKG in $AUR_PCKGS; do
-        yay --sudoloop --needed --noconfirm -S "$PCKG" || error "Error installing $PCKG"
-    done
+    install_yay_packages "$common_aur_packages"
 }
 
 ## GIT PACKAGES ##
@@ -60,50 +116,85 @@ git_packages() {
         error "Error installing dotfiles"
     fi
 
-    # dwm
-    if git clone https://github.com/saghya/dwm ~/.local/src/dwm; then
-        cd ~/.local/src/dwm &&
-        make
-        sudo make install
-    else
-        error "Error installing dwm"
+    if install_xorg; then
+        # dwm
+        if git clone https://github.com/saghya/dwm ~/.local/src/dwm; then
+            cd ~/.local/src/dwm &&
+            make
+            sudo make install
+        else
+            error "Error installing dwm"
+        fi
+
+        # dwmblocks-async
+        if git clone https://github.com/UtkarshVerma/dwmblocks-async ~/.local/src/dwmblocks-async; then
+            cd ~/.local/src/dwmblocks-async &&
+            printf "%s\n"                                              \
+                   "#ifndef CONFIG_H"                                  \
+                   "#define CONFIG_H"                                  \
+                   ""                                                  \
+                   "#define DELIMITER              \"\""               \
+                   "#define MAX_BLOCK_OUTPUT_LENGTH 50"                \
+                   "#define CLICKABLE_BLOCKS         1"                \
+                   "#define LEADING_DELIMITER        0"                \
+                   "#define TRAILING_DELIMITER       0"                \
+                   ""                                                  \
+                   "#define BLOCKS(X) \\"                              \
+                   "	X(\"\", \"sb-volume\",            1,    1) \\" \
+                   "	X(\"\", \"sb-battery\",          30,    4) \\" \
+                   "	X(\"\", \"sb-network\",          15,    5) \\" \
+                   "	X(\"\", \"sb-bluetooth\",        15,    6) \\" \
+                   "	X(\"\", \"sb-date\",             10,    7) \\" \
+                   "	X(\"\", \"sb-powermenu_icon\",   10,    8) \\" \
+                   ""                                                  \
+                   "#endif // CONFIG_H" > config.h
+            make
+            sudo make install
+        else
+            error "Error installing dwmblocks-async"
+        fi
+
+        # dmenu
+        if git clone https://github.com/saghya/dmenu ~/.local/src/dmenu; then
+            cd ~/.local/src/dmenu &&
+            make
+            sudo make install
+        else
+            error "Error installing dmenu"
+        fi
     fi
 
-    # dwmblocks-async
-    if git clone https://github.com/UtkarshVerma/dwmblocks-async ~/.local/src/dwmblocks-async; then
-        cd ~/.local/src/dwmblocks-async &&
-        printf "%s\n"                                              \
-               "#ifndef CONFIG_H"                                  \
-               "#define CONFIG_H"                                  \
-               ""                                                  \
-               "#define DELIMITER              \"\""               \
-               "#define MAX_BLOCK_OUTPUT_LENGTH 50"                \
-               "#define CLICKABLE_BLOCKS         1"                \
-               "#define LEADING_DELIMITER        0"                \
-               "#define TRAILING_DELIMITER       0"                \
-               ""                                                  \
-               "#define BLOCKS(X) \\"                              \
-               "	X(\"\", \"sb-volume\",            1,    1) \\" \
-               "	X(\"\", \"sb-battery\",          30,    4) \\" \
-               "	X(\"\", \"sb-network\",          15,    5) \\" \
-               "	X(\"\", \"sb-bluetooth\",        15,    6) \\" \
-               "	X(\"\", \"sb-date\",             10,    7) \\" \
-               "	X(\"\", \"sb-powermenu_icon\",   10,    8) \\" \
-               ""                                                  \
-               "#endif // CONFIG_H" > config.h
-        make
-        sudo make install
-    else
-        error "Error installing dwmblocks-async"
-    fi
+    if install_wayland; then
+        # dwl
+        if git clone https://github.com/saghya/dwl ~/.local/src/dwl; then
+            cd ~/.local/src/dwl &&
+            make
+            sudo make install
+        else
+            error "Error installing dwl"
+        fi
 
-    # dmenu
-    if git clone https://github.com/saghya/dmenu ~/.local/src/dmenu; then
-        cd ~/.local/src/dmenu &&
-        make
-        sudo make install
-    else
-        error "Error installing dmenu"
+        # slstatus
+        if git clone https://git.suckless.org/slstatus ~/.local/src/slstatus; then
+            cd ~/.local/src/slstatus &&
+            printf "%s\n"                                           \
+                   "const unsigned int interval = 1000;"            \
+                   "static const char unknown_str[] = \"\";"        \
+                   "#define MAXLEN 2048"                            \
+                   ""                                               \
+                   "static const struct arg args[] = {"             \
+                   "    { run_command, \"%s\", \"sl-volume\" },"    \
+                   "    { run_command, \"%s\", \"sl-battery\" },"   \
+                   "    { run_command, \"%s\", \"sl-bluetooth\" }," \
+                   "    { run_command, \"%s\", \"sl-network\" },"   \
+                   "    { run_command, \"%s\", \"sl-date\" },"      \
+                   "};"                                             \
+                   "" > config.def.h
+            make
+            sudo make install
+        else
+            error "Error installing slstatus"
+        fi
     fi
 
     # afetch
@@ -118,10 +209,14 @@ git_packages() {
 
 ## LAPTOP ##
 laptop() {
-    L_PCKGS="brightnessctl tlp tlp-rdw tlpui batsignal libinput-gestures"
+    L_PCKGS="brightnessctl tlp tlp-rdw tlpui batsignal"
     for PCKG in $L_PCKGS; do
         yay --sudoloop --needed --noconfirm -S "$PCKG" || error "Error installing $PCKG"
     done
+
+    if install_xorg; then
+        yay --sudoloop --needed --noconfirm -S libinput-gestures || error "Error installing libinput-gestures"
+    fi
 
     # tlp
     sudo systemctl enable tlp.service
@@ -151,39 +246,42 @@ laptop() {
 
     # touchpad
     sudo usermod -a -G input "$USER"
-    sudo touch /etc/X11/xorg.conf.d/30-touchpad.conf
-    printf "%s\n"                                     \
-           "Section \"InputClass\""                   \
-           "    Identifier \"mytouchpad\""            \
-           "    Driver \"libinput\""                  \
-           "    MatchIsTouchpad \"on\""               \
-           "    Option \"NaturalScrolling\" \"true\"" \
-           "    Option \"Tapping\" \"true\""          \
-           "EndSection"                               |
-    sudo tee /etc/X11/xorg.conf.d/30-touchpad.conf
-    
-    # acpi events
-    sudo touch /etc/acpi/events/ac_adapter
-    printf "%s\n"                            \
-           "event=ac_adapter"                \
-           "action=pkill -RTMIN+4 dwmblocks" |
-    sudo tee /etc/acpi/events/ac_adapter
-    
-    # brightness and video group
     sudo usermod -a -G video "$USER"
     sudo touch /etc/udev/rules.d/backlight.rules
     echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", KERNEL==\"acpi_video0\", GROUP=\"video\", MODE=\"0664\"" |
     sudo tee /etc/udev/rules.d/backlight.rules
+
+    if install_xorg; then
+        sudo touch /etc/X11/xorg.conf.d/30-touchpad.conf
+        printf "%s\n"                                     \
+               "Section \"InputClass\""                   \
+               "    Identifier \"mytouchpad\""            \
+               "    Driver \"libinput\""                  \
+               "    MatchIsTouchpad \"on\""               \
+               "    Option \"NaturalScrolling\" \"true\"" \
+               "    Option \"Tapping\" \"true\""          \
+               "EndSection"                               |
+        sudo tee /etc/X11/xorg.conf.d/30-touchpad.conf
+
+        # acpi events
+        sudo touch /etc/acpi/events/ac_adapter
+        printf "%s\n"                            \
+               "event=ac_adapter"                \
+               "action=pkill -RTMIN+4 dwmblocks" |
+        sudo tee /etc/acpi/events/ac_adapter
+    fi
 }
 
 services() {
     # acpi events
     sudo systemctl enable acpid.service
-    sudo touch /etc/acpi/events/jack
-    printf "%s\n"                            \
-           "event=jack*"                     \
-           "action=pkill -RTMIN+1 dwmblocks" |
-    sudo tee /etc/acpi/events/jack
+    if install_xorg; then
+        sudo touch /etc/acpi/events/jack
+        printf "%s\n"                            \
+               "event=jack*"                     \
+               "action=pkill -RTMIN+1 dwmblocks" |
+        sudo tee /etc/acpi/events/jack
+    fi
 
     # bluetooth
     sudo systemctl enable bluetooth.service
@@ -214,18 +312,32 @@ services() {
 }
 
 finishing_touches() {
-    # dwm login session
-    sudo mkdir -p /usr/share/xsessions
-    sudo touch /usr/share/xsessions/dwm.desktop
-    printf "%s\n"                           \
-           "[Desktop Entry]"                \
-           "Encoding=UTF-8"                 \
-           "Name=dwm"                       \
-           "Comment=Dynamic window manager" \
-           "Exec=startdwm"                  \
-           "Icon=dwm"                       \
-           "Type=XSession"                  |
-    sudo tee /usr/share/xsessions/dwm.desktop
+    if install_xorg; then
+        # dwm login session
+        sudo mkdir -p /usr/share/xsessions
+        sudo touch /usr/share/xsessions/dwm.desktop
+        printf "%s\n"                           \
+               "[Desktop Entry]"                \
+               "Encoding=UTF-8"                 \
+               "Name=dwm"                       \
+               "Comment=Dynamic window manager" \
+               "Exec=startdwm"                  \
+               "Icon=dwm"                       \
+               "Type=XSession"                  |
+        sudo tee /usr/share/xsessions/dwm.desktop
+    fi
+
+    if install_wayland; then
+        sudo mkdir -p /usr/share/wayland-sessions
+        sudo touch /usr/share/wayland-sessions/dwl.desktop
+        printf "%s\n"                    \
+               "[Desktop Entry]"         \
+               "Name=dwl"                \
+               "Comment=dwm for Wayland" \
+               "Exec=startdwl"           \
+               "Type=Application"        |
+        sudo tee /usr/share/wayland-sessions/dwl.desktop
+    fi
 
     # default text editor
     sudo mkdir -p ~/.local/share/applications
@@ -268,10 +380,12 @@ finishing_touches() {
     # give the home directory to the user
     sudo chown -R "$USER" "$HOME"
 
-    # install dwmblocks helper
-    cd ~/.local/scripts/helpers/get-monitor-x &&
-    make
-    sudo make install
+    if install_xorg; then
+        # install dwmblocks helper
+        cd ~/.local/scripts/xorg/helpers/get-monitor-x &&
+        make
+        sudo make install
+    fi
 
     # errors
     if [ -f ~/.install-errors.log ]; then
@@ -284,6 +398,8 @@ finishing_touches() {
 }
 
 main() {
+    choose_display_stack "${1:-}"
+
     sudo -v || exit 1
     while true; do
         sudo -n true || exit
@@ -307,5 +423,5 @@ main() {
     finishing_touches
 }
 
-main
+main "$@"
 
